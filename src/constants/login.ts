fc743e6a68f520d7b19189d640b75f7a8ad06437
Matheus Paice <matheus.paice@jbtec.com.br>
2023-12-07 11:11:50 -0300

    feat: set template id

--- src/constants/login.ts
+++ src/constants/login.ts
@@ -1,5 +1,7 @@
 export const LOGIN_TEMPLATE_EMAIL_SIGNIN =
-    process.env.LOGIN_TEMPLATE_EMAIL_SIGNIN || '';
+    process.env.LOGIN_TEMPLATE_EMAIL_SIGNIN ||
+    'd-631740bd4eae4beea96b97b83def0bf7';
 
 export const LOGIN_TEMPLATE_EMAIL_SIGNUP =
-    process.env.LOGIN_TEMPLATE_EMAIL_SIGNUP || '';
+    process.env.LOGIN_TEMPLATE_EMAIL_SIGNUP ||
+    'd-3726ed28f0f442d791738af64ccd3adf';
