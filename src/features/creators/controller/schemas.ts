bc988cb647053962d296cacdbe1d1233e1514685
Matheus Paice <matheus.paice@jbtec.com.br>
2023-12-20 18:52:10 -0300

    fix: correct rule validate body update creator

--- src/features/creators/controller/schemas.ts
+++ src/features/creators/controller/schemas.ts
@@ -4,11 +4,26 @@ export const schemaValidationForCreate = z.object({});
 export const schemaValidationForPut = z.object({
     name: z.string(),
     username: z.string(),
+    wallets: z.array(
+        z.object({
+            address: z.string(),
+            network: z.object({
+                name: z.string(),
+                chainId: z.number(),
+            }),
+        })
+    ),
     profile: z.object({
         avatar: z.string().nullable().default(null),
         phone: z.string().nullable().default(null),
         language: z.string().nullable().default(null),
         location: z.string().nullable().default(null),
+    }),
+    framework: z.object({
+        createdAt: z.string().nullable(),
+        createdBy: z.string().nullable(),
+        updatedAt: z.string().nullable(),
+        updatedBy: z.string().nullable(),
     }),
 });
 export const schemaValidationForAddEmail = z.object({
