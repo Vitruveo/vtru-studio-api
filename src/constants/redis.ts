0c197765cccada2ae7fc2211b25b992b0f245cec
Matheus Paice <matheus.paice@jbtec.com.br>
2024-04-15 15:20:19 -0300

    fix: force build

--- src/constants/redis.ts
+++ src/constants/redis.ts
@@ -5,4 +5,4 @@ export const REDIS_MAIN_CLUSTER = process.env.REDIS_MAIN_CLUSTER
 export const REDIS_MAIN_PASSWORD =
     process.env.REDIS_MAIN_PASSWORD || 'password';
 export const REDIS_MAIN_IN_CLUSTER = REDIS_MAIN_CLUSTER.length > 1;
-// #endregion
+// #endregion.
