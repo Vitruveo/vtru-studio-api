1557405b125e607b3b02ea7c5590dbf2e9b1003e
Nelio Santos <nsfilho@icloud.com>
2024-04-22 16:48:07 -0300

    refactor: small changes in rabbitmq

--- src/constants/express.ts
+++ src/constants/express.ts
@@ -1,4 +1,4 @@
 export const EXPRESS_PORT = process.env.EXPRESS_PORT || 3000;
 export const EXPRESS_START_DELAY = process.env.EXPRESS_START_DELAY
     ? parseInt(process.env.EXPRESS_START_DELAY, 10)
-    : 5000;
+    : 15_000;
