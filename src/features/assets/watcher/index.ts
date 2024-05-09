0e5b8d62c269805c3a6084e5896b33b6a3ed9a58
Matheus Paice <matheus.paice@jbtec.com.br>
2024-05-09 19:30:09 -0300

    fix: correct watcher assets change status

--- src/features/assets/watcher/index.ts
+++ src/features/assets/watcher/index.ts
@@ -1,9 +1,10 @@
 import debug from 'debug';
 import { uniqueExecution } from '@nsfilho/unique';
-import { exitWithDelay, retry } from '../../../utils';
-import { captureException, getDb } from '../../../services';
-import { COLLECTION_ASSETS } from '../model';
+import { COLLECTION_ASSETS, AssetsDocument } from '../model';
+import { ASSET_STORAGE_URL, STORE_URL } from '../../../constants';
+import { captureException, getDb, ObjectId } from '../../../services';
 import { sendToExchangeRSS } from '../../../services/rss';
+import { exitWithDelay, retry } from '../../../utils';
 
 const logger = debug('features:assets:watcher');
 
@@ -15,13 +16,13 @@ uniqueExecution({
                 logger('Watching changes in assets');
 
                 const changeStream = getDb()
-                    .collection(COLLECTION_ASSETS)
+                    .collection<AssetsDocument>(COLLECTION_ASSETS)
                     .watch([], { fullDocument: 'updateLookup' });
 
-                changeStream.on('change', (change) => {
+                changeStream.on('change', async (change) => {
                     if (change.operationType === 'delete') {
                         // dispatch queue to remove asset from rss
-                        sendToExchangeRSS(
+                        await sendToExchangeRSS(
                             JSON.stringify({
                                 id: change.documentKey._id.toString(),
                             }),
@@ -33,20 +34,79 @@ uniqueExecution({
                         change.operationType === 'replace' ||
                         change.operationType === 'update'
                     ) {
-                        // check consign artwork status
+                        // check consign artwork status before is active and after is not active
                         if (
-                            change.fullDocumentBeforeChange?.consignArtwork
-                                .status !==
-                                change.fullDocument?.consignArtwork.status &&
-                            change.fullDocument?.consignArtwork.status !==
+                            change.fullDocument?.consignArtwork?.status &&
+                            change.fullDocument?.consignArtwork?.status !==
                                 'active'
                         ) {
                             // dispatch queue to remove from rss
-                            sendToExchangeRSS(
+                            await sendToExchangeRSS(
                                 JSON.stringify({
                                     id: change.documentKey._id.toString(),
                                 }),
                                 'remove'
+                            );
+                        }
+
+                        // check consign artwork status before is not active and after is active
+                        if (
+                            change.fullDocument?.consignArtwork?.status &&
+                            change.fullDocument?.consignArtwork?.status ===
+                                'active'
+                        ) {
+                            const asset = change.fullDocument;
+                            if (!asset.framework.createdBy) return;
+
+                            const creator = await getDb()
+                                .collection('creators')
+                                .findOne(
+                                    {
+                                        _id: new ObjectId(
+                                            asset.framework.createdBy
+                                        ),
+                                    },
+                                    { projection: { username: 1 } }
+                                );
+
+                            if (!creator) return;
+
+                            await Promise.all(
+                                Object.entries(asset.licenses).map(
+                                    ([key, license]) => {
+                                        if (license.added) {
+                                            const payload = JSON.stringify({
+                                                license: key,
+                                                id: asset._id.toString(),
+                                                title: asset.assetMetadata
+                                                    .context.formData.title,
+                                                url: `${STORE_URL}/${
+                                                    creator.username
+                                                }/${asset._id.toString()}/${Date.now()}`,
+                                                creator:
+                                                    asset.assetMetadata.creators
+                                                        .formData[0].name,
+                                                image: `${ASSET_STORAGE_URL}/${asset.formats.preview?.path}`,
+                                                description:
+                                                    asset?.mediaAuxiliary
+                                                        ?.description ||
+                                                    asset.assetMetadata.context
+                                                        .formData.description,
+                                            });
+                                            // dispatch queue to add to rss
+                                            return sendToExchangeRSS(
+                                                payload,
+                                                'consign'
+                                            );
+                                        }
+                                        return Promise.resolve();
+                                    }
+                                )
+                            ).catch((error) =>
+                                logger(
+                                    'Error sending to exchange rss: %O',
+                                    error
+                                )
                             );
                         }
                     }
