bb45bdcc2c6573d40cc3d16a3cd225c1b1d165bd
robertorf077 <joseroberto@moki.com.br>
2024-07-22 16:09:21 -0300

    Feat: Validar Midias

--- src/features/assets/controller/consign.ts
+++ src/features/assets/controller/consign.ts
@@ -1,22 +1,45 @@
 import debug from 'debug';
 import { nanoid } from 'nanoid';
 import { Router } from 'express';
+import { S3Client, HeadObjectCommand } from '@aws-sdk/client-s3';
+import { ObjectId, APIResponse, captureException } from '../../../services';
 import { ZodError } from 'zod';
 
 import * as model from '../model';
 import * as modelCreators from '../../creators/model';
 import { middleware } from '../../users';
-import { APIResponse, ObjectId, captureException } from '../../../services';
+import { formatErrorMessage } from '../../../utils';
+import {
+    AWS_DEFAULT_REGION,
+    GENERAL_STORAGE_URL,
+    DEFAULT_AVATAR_URL,
+    ASSET_STORAGE_NAME,
+} from '../../../constants';
 import {
     schemaAssetValidation,
     schemaCreatorValidation,
 } from './schemaValidate';
-import { formatErrorMessage } from '../../../utils';
 
 const logger = debug('features:assets:controller:consign');
+const s3Client = new S3Client({ region: AWS_DEFAULT_REGION });
+const bucketName = ASSET_STORAGE_NAME;
 const route = Router();
 
 route.use(middleware.checkAuth);
+
+async function checkS3FileExists(key: string) {
+    try {
+        await s3Client.send(
+            new HeadObjectCommand({ Bucket: bucketName, Key: key })
+        );
+        return true;
+    } catch (error: any) {
+        if (error.name === 'NotFound') {
+            return false;
+        }
+        throw error; 
+    }
+}
 
 route.get('/validation/:id', async (req, res) => {
     try {
@@ -36,8 +59,47 @@ route.get('/validation/:id', async (req, res) => {
         }
 
         const creator = await modelCreators.findCreatorById({
-            id: asset?.framework.createdBy,
+            id: asset.framework.createdBy,
         });
+
+        if (!creator) {
+            return res.status(404).json({
+                code: 'vitruveo.studio.api.assets.Creator not found',
+                message: 'Creator not found',
+                transaction: nanoid(),
+            } as APIResponse);
+        }
+
+        const avatarPath =
+            creator.profile?.avatar &&
+            creator.profile.avatar !== DEFAULT_AVATAR_URL
+                ? `${GENERAL_STORAGE_URL}/${creator.profile.avatar}`
+                : null;
+
+        const mediaPaths = [
+            asset.formats?.original?.path,
+            asset.formats?.display?.path,
+            asset.formats?.preview?.path,
+            asset.formats?.exhibition?.path,
+            asset.formats?.print?.path,
+            avatarPath,
+        ]
+            .filter(Boolean)
+            .map((path) => {
+                return path ? path.replace(GENERAL_STORAGE_URL + '/', '') : '';
+            })
+            .filter(Boolean);
+
+        try {
+            await Promise.all(mediaPaths.map(checkS3FileExists));
+        } catch (error) {
+            return res.status(404).json({
+                code: 'vitruveo.studio.api.assets.consign.validation.error',
+                message: `Media file at path does not exist in our S3 bucket.`,
+                transaction: nanoid(),
+            } as APIResponse);
+        }
+
         schemaCreatorValidation.parse(creator);
         schemaAssetValidation.parse(asset);
 
