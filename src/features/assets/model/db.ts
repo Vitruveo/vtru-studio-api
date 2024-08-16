6c46db17d3c73caf3a6be51cbc811950fd7788f0
Matheus Paice <matheus.paice@jbtec.com.br>
2024-08-16 11:21:08 -0300

    feat: add route scope assets by address

--- src/features/assets/model/db.ts
+++ src/features/assets/model/db.ts
@@ -23,6 +23,7 @@ import type {
 import { FindOptions, getDb, ObjectId } from '../../../services/mongo';
 import { conditionsToShowAssets } from '../controller/public';
 import { buildFilterColorsQuery } from '../utils/color';
+import { ASSET_STORAGE_URL, STORE_URL } from '../../../constants';
 
 const assets = () => getDb().collection<AssetsDocument>(COLLECTION_ASSETS);
 
@@ -359,6 +360,62 @@ export const findAssetsById = async ({ id }: FindAssetsByIdParams) => {
     const result = await assets().findOne({ _id: new ObjectId(id) });
     return result;
 };
+
+export const findAssetMintedByAddress = async ({
+    address,
+}: {
+    address: string;
+}) =>
+    assets()
+        .aggregate([
+            {
+                $match: {
+                    'mintExplorer.address': address,
+                    'framework.createdBy': { $exists: true, $ne: '' },
+                },
+            },
+            {
+                $addFields: {
+                    creatorId: {
+                        $toObjectId: '$framework.createdBy',
+                    },
+                },
+            },
+            {
+                $lookup: {
+                    from: 'creators',
+                    localField: 'creatorId',
+                    foreignField: '_id',
+                    as: 'creator',
+                },
+            },
+            {
+                $unwind: {
+                    path: '$creator',
+                },
+            },
+            {
+                $project: {
+                    storeUrl: {
+                        $concat: [
+                            STORE_URL,
+                            '/',
+                            '$creator.username',
+                            '/',
+                            '$consignArtwork.assetKey',
+                        ],
+                    },
+                    previewUrl: {
+                        $concat: [
+                            ASSET_STORAGE_URL,
+                            '/',
+                            '$formats.preview.path',
+                        ],
+                    },
+                },
+            },
+        ])
+        .toArray();
 
 export const findAssetsByCreatorId = async ({ id }: FindAssetsByIdParams) =>
     assets()
