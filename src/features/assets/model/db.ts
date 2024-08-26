1b8964ecb7c7f71e8cd88ba82b6890f31e2738e6
Matheus Paice <paice.zinner@gmail.com>
2024-08-26 11:57:54 -0300

    fix: correct group assets

--- src/features/assets/model/db.ts
+++ src/features/assets/model/db.ts
@@ -75,6 +75,7 @@ export const findAssetGroupPaginated = ({
                             $expr: {
                                 $eq: ['$framework.createdBy', '$$creatorId'],
                             },
+                            ...query
                         },
                     },
                     {
