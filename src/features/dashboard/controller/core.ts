fe40c7a3bba514c718069bd6adadcd9fbe355ca2
Luan Almeida <luan.almeida@jbtec.com.br>
2024-09-26 20:57:37 -0300

    feat: filter by date

--- src/features/dashboard/controller/core.ts
+++ src/features/dashboard/controller/core.ts
@@ -11,31 +11,72 @@ const route = Router();
 
 route.get('/', async (req, res) => {
     try {
-        const creators = await countAllCreators();
+        const date = req.query.date as string;
+        const start = req.query.start as string;
+        const end = req.query.end as string;
+
+        const dateFormatted = date
+            ? new Date(`${date}T00:00:00.000Z`).toISOString()
+            : null;
+        const startFormatted = start
+            ? new Date(`${start}T00:00:00.000Z`).toISOString()
+            : null;
+        const endFormatted = end
+            ? new Date(`${end}T23:59:59.999Z`).toISOString()
+            : null;
+
+        const buildQuery = (key: string) => {
+            let query = {};
+            if (dateFormatted) {
+                query = {
+                    [key]: {
+                        $gte: dateFormatted,
+                        $lt: new Date(
+                            new Date(dateFormatted).getTime() +
+                                24 * 60 * 60 * 1000
+                        ).toISOString(),
+                    },
+                };
+            } else if (startFormatted && endFormatted) {
+                query = {
+                    [key]: {
+                        $gte: startFormatted,
+                        $lt: endFormatted,
+                    },
+                };
+            }
+            return query;
+        };
+
+        const creators = await countAllCreators({
+            ...buildQuery('vault.createdAt'),
+        });
         const arts = await countAllAssets();
         const consigned = await countAllAssets({
             contractExplorer: { $exists: true },
+            ...buildQuery('consignArtwork.listing'),
         });
         const activeConsigned = await countAllAssets({
             contractExplorer: { $exists: true },
             'consignArtwork.status': 'active',
+            ...buildQuery('consignArtwork.listing'),
         });
         const totalPrice = await getTotalPrice();
         const artsSold = await countAllAssets({
             mintExplorer: { $exists: true },
+            ...buildQuery('mintExplorer.createdAt'),
         });
-
         const averagePrice = totalPrice / artsSold;
 
         await sendMessageDiscord({
             message: `Vitruveo Dashboard:\n 
-creators: ${creators}
-arts: ${arts}
-consigned: ${consigned}
-activeConsigned: ${activeConsigned}
-totalPrice: ${totalPrice}
-artsSold: ${artsSold}
-averagePrice: ${averagePrice}`,
+                creators: ${creators}
+                arts: ${arts}
+                consigned: ${consigned}
+                activeConsigned: ${activeConsigned}
+                totalPrice: ${totalPrice}
+                artsSold: ${artsSold}
+                averagePrice: ${averagePrice}`,
         });
 
         const response = {
