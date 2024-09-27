416d775c7605037d277387be286a8e67b19c84b3
Luan Almeida <luan.almeida@jbtec.com.br>
2024-09-27 15:12:14 -0300

    feat: filter by date

--- src/features/dashboard/controller/core.ts
+++ src/features/dashboard/controller/core.ts
@@ -1,10 +1,16 @@
 import debug from 'debug';
 import { nanoid } from 'nanoid';
 import { Router } from 'express';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 import { APIResponse } from '../../../services';
 import { countAllCreators } from '../../creators/model';
 import { countAllAssets, getTotalPrice } from '../../assets/model';
-import { sendMessageDiscord } from '../../../services/discord';
+// import { sendMessageDiscord } from '../../../services/discord';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const logger = debug('features:dashboard:controller');
 const route = Router();
@@ -12,81 +18,107 @@ const route = Router();
 route.get('/', async (req, res) => {
     try {
         const date = req.query.date as string;
-        const start = req.query.start as string;
-        const end = req.query.end as string;
 
         const dateFormatted = date
-            ? new Date(`${date}T00:00:00.000Z`).toISOString()
-            : null;
-        const startFormatted = start
-            ? new Date(`${start}T00:00:00.000Z`).toISOString()
-            : null;
-        const endFormatted = end
-            ? new Date(`${end}T23:59:59.999Z`).toISOString()
-            : null;
+            ? dayjs(date).utc().endOf('day').toDate()
+            : dayjs().utc().endOf('day').toDate();
+        const yesterdayFormatted = date
+            ? dayjs(date).utc().subtract(1, 'day').startOf('day').toDate()
+            : dayjs().utc().subtract(1, 'day').startOf('day').toDate();
 
-        const buildQuery = (key: string) => {
-            let query = {};
-            if (dateFormatted) {
-                query = {
+        const buildQuery = (key: string, prop: 'total' | 'new') => ({
+            $or: [
+                {
                     [key]: {
-                        $gte: dateFormatted,
-                        $lt: new Date(
-                            new Date(dateFormatted).getTime() +
-                                24 * 60 * 60 * 1000
-                        ).toISOString(),
+                        $gte:
+                            prop === 'total'
+                                ? dayjs(0).utc().toDate()
+                                : yesterdayFormatted,
+                        $lt: dateFormatted,
                     },
-                };
-            } else if (startFormatted && endFormatted) {
-                query = {
+                },
+                {
                     [key]: {
-                        $gte: startFormatted,
-                        $lt: endFormatted,
+                        $gte:
+                            prop === 'total'
+                                ? dayjs(0).utc().toISOString()
+                                : yesterdayFormatted.toISOString(),
+                        $lt: dateFormatted.toISOString(),
                     },
-                };
-            }
-            return query;
-        };
-
-        const creators = await countAllCreators({
-            ...buildQuery('vault.createdAt'),
+                },
+            ],
         });
-        const arts = await countAllAssets();
-        const consigned = await countAllAssets({
+
+        const totalCreators = await countAllCreators({
+            ...buildQuery('framework.createdAt', 'total'),
+        });
+        const newCreators = await countAllCreators({
+            ...buildQuery('framework.createdAt', 'new'),
+        });
+        const totalArts = await countAllAssets({
+            ...buildQuery('framework.createdAt', 'total'),
+        });
+        const newArts = await countAllAssets({
+            ...buildQuery('framework.createdAt', 'new'),
+        });
+        const totalConsigned = await countAllAssets({
             contractExplorer: { $exists: true },
-            ...buildQuery('consignArtwork.listing'),
+            ...buildQuery('consignArtwork.listing', 'total'),
         });
-        const activeConsigned = await countAllAssets({
+        const newConsigned = await countAllAssets({
+            contractExplorer: { $exists: true },
+            ...buildQuery('consignArtwork.listing', 'new'),
+        });
+        const totalActiveConsigned = await countAllAssets({
             contractExplorer: { $exists: true },
             'consignArtwork.status': 'active',
-            ...buildQuery('consignArtwork.listing'),
+            ...buildQuery('consignArtwork.listing', 'total'),
+        });
+        const newActiveConsigned = await countAllAssets({
+            contractExplorer: { $exists: true },
+            'consignArtwork.status': 'active',
+            ...buildQuery('consignArtwork.listing', 'new'),
+        });
+        const totalArtsSold = await countAllAssets({
+            mintExplorer: { $exists: true },
+            ...buildQuery('mintExplorer.createdAt', 'total'),
+        });
+        const newArtsSold = await countAllAssets({
+            mintExplorer: { $exists: true },
+            ...buildQuery('mintExplorer.createdAt', 'new'),
         });
         const totalPrice = await getTotalPrice();
-        const artsSold = await countAllAssets({
-            mintExplorer: { $exists: true },
-            ...buildQuery('mintExplorer.createdAt'),
-        });
-        const averagePrice = totalPrice / artsSold;
+        const averagePrice = totalPrice / totalArtsSold;
+        const periodTotalPrice = '';
+        const periodAveragePrice = '';
 
-        await sendMessageDiscord({
-            message: `Vitruveo Dashboard:\n 
-                creators: ${creators}
-                arts: ${arts}
-                consigned: ${consigned}
-                activeConsigned: ${activeConsigned}
-                totalPrice: ${totalPrice}
-                artsSold: ${artsSold}
-                averagePrice: ${averagePrice}`,
-        });
+        // await sendMessageDiscord({
+        //     message: `Vitruveo Dashboard:\n
+        //         creators: ${creators}
+        //         arts: ${arts}
+        //         consigned: ${consigned}
+        //         activeConsigned: ${activeConsigned}
+        //         totalPrice: ${totalPrice}
+        //         artsSold: ${artsSold}
+        //         averagePrice: ${averagePrice}`,
+        // });
 
         const response = {
-            creators,
-            arts,
-            consigned,
-            activeConsigned,
+            date,
+            totalCreators,
+            newCreators,
+            totalArts,
+            newArts,
+            totalConsigned,
+            newConsigned,
+            totalActiveConsigned,
+            newActiveConsigned,
+            totalArtsSold,
+            newArtsSold,
             totalPrice,
-            artsSold,
             averagePrice,
+            periodTotalPrice,
+            periodAveragePrice,
         };
 
         res.json({
