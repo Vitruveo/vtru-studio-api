6daa114ca823a32fe518327636ec97f3a2688664
luan almeida <luanbianchine1@gmail.com>
2024-11-14 08:34:14 -0300

    fix: zod schema rules

--- src/features/stores/controller/schemas.ts
+++ src/features/stores/controller/schemas.ts
@@ -1,27 +1,22 @@
 import { z } from 'zod';
 import { FormatSchema } from '../model';
 
-export const schemaValidationForCreateStores = z
-    .object({
-        organization: z
-            .object({
-                // rules lower case a - z and 0 - 9 and hyphen and min 4 char
-                url: z.string().nullable().default(null),
-                name: z.string().default(''),
-                description: z.string().nullable().default(null),
-                markup: z.number().default(0),
-                formats: FormatSchema.default({
-                    logo: { horizontal: null, square: null },
-                    banner: null,
-                }),
-            })
-            .default({})
-            .optional(),
-        cloneId: z.string().optional(),
-    })
-    .refine((data) => data.organization || data.cloneId, {
-        message: 'organization or cloneId is required',
-    });
+export const schemaValidationForCreateStores = z.object({
+    organization: z
+        .object({
+            // rules lower case a - z and 0 - 9 and hyphen and min 4 char
+            url: z.string().nullable().default(null),
+            name: z.string().default(''),
+            description: z.string().nullable().default(null),
+            markup: z.number().default(0),
+            formats: FormatSchema.default({
+                logo: { horizontal: null, square: null },
+                banner: null,
+            }),
+        })
+        .default({}),
+    cloneId: z.string().optional(),
+});
 
 export const schemaValidationOrganization = z.object({
     url: z.string().nullable().default(null),
