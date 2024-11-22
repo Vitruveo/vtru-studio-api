8df60a878b5b65787e0e1273790bf3e151b59cdd
Matheus Paice <paice.zinner@gmail.com>
2024-11-22 16:21:06 -0300

    feat: set hash to subdomain

--- src/features/stores/controller/index.ts
+++ src/features/stores/controller/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { route as coreRouter } from './core';
 import { route as uploadRouter } from './upload';
+import { route as publicRouter } from './public';
 
 const router = Router();
 
-router.use('/stores', coreRouter);
 router.use('/stores/upload', uploadRouter);
+router.use('/stores/public', publicRouter);
+router.use('/stores', coreRouter);
 
 export { router };
