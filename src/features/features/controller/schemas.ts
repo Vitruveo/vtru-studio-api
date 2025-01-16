e750cdea79f5217bc2bec1f0ae0dcb80d851dc37
marcos <marcos.barboza@jbtec.com.br>
2025-01-16 09:19:40 -0300

    fix: features schema

--- src/features/features/controller/schemas.ts
+++ src/features/features/controller/schemas.ts
@@ -13,7 +13,9 @@ export const schemaValidationForCreate = z.object({
 export const schemaValidationForUpdate = z.object({
     name: z.string(),
     released: z.boolean().optional(),
-    onlyForAllowList: z.boolean().optional(),
+    isOnlyFor: z.boolean().optional(),
+    onlyFor: z.enum(['allowList', 'specificUsers']).optional(),
+    emails: z.array(z.string()).optional(),
     framework: z.object({
         createdAt: z.string().or(z.date()).nullable().default(null),
         updatedAt: z.string().or(z.date()).nullable().default(null),
