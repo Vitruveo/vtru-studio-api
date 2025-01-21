a3c7fdd5ea9edfef8fbec128064a7002b6a35f4f
Luan Almeida <luan.almeida@jbtec.com.br>
2025-01-21 18:32:29 -0300

    feat: route to return last 5 consings

--- src/features/assets/model/db.ts
+++ src/features/assets/model/db.ts
@@ -34,6 +34,7 @@ import type {
     FindAssetsWithArtCardsPaginatedParams,
     UpdateAssetArtCardsStatusParams,
     CountAssetsWithLicenseArtCardsParams,
+    FindLastConsignsParams,
 } from './types';
 import { FindOptions, getDb, ObjectId } from '../../../services/mongo';
 import { buildFilterColorsQuery } from '../utils/color';
@@ -750,10 +751,29 @@ export const findAssets = async ({
     return result.stream();
 };
 
-export const findAssetsById = async ({ id }: FindAssetsByIdParams) => {
-    const result = await assets().findOne({ _id: new ObjectId(id) });
-    return result;
-};
+export const findAssetsById = async ({ id }: FindAssetsByIdParams) =>
+    assets().findOne({ _id: new ObjectId(id) });
+
+export const findLastConsigns = async ({ id }: FindLastConsignsParams) =>
+    assets()
+        .aggregate([
+            {
+                $match: {
+                    'consignArtwork.status': 'active',
+                    'framework.createdBy': id,
+                    mintExplorer: { $exists: false },
+                },
+            },
+            { $sort: { 'consignArtwork.createdAt': -1 } },
+            { $limit: 5 },
+            {
+                $project: {
+                    id: '$_id',
+                    path: '$formats.preview.path',
+                },
+            },
+        ])
+        .toArray();
 
 export const countAssetsWithLicenseArtCards = async ({
     status,
