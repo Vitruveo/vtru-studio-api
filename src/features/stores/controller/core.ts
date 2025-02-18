20e6b3f4c587b0f54044eca8be93c44efbb68606
Luan Almeida <luan.almeida@jbtec.com.br>
2025-02-18 12:12:55 -0300

    feat: each situation with personalized message

--- src/features/stores/controller/core.ts
+++ src/features/stores/controller/core.ts
@@ -376,7 +376,7 @@ route.post('/validateUrl/:id', async (req, res) => {
         const checkUrl = await isValidUrl(url);
         if (checkUrl === 'reservedWords') {
             res.json({
-                code: 'vitruveo.studio.api.stores.create.failed',
+                code: 'vitruveo.studio.api.stores.validateUrl.failed',
                 message: 'URL contains a reserved word',
                 transaction: nanoid(),
                 data: false,
@@ -385,7 +385,7 @@ route.post('/validateUrl/:id', async (req, res) => {
         }
         if (checkUrl === 'characters') {
             res.json({
-                code: 'vitruveo.studio.api.stores.create.failed',
+                code: 'vitruveo.studio.api.stores.validateUrl.failed',
                 message: 'URL format is invalid',
                 transaction: nanoid(),
                 data: false,
@@ -397,12 +397,21 @@ route.post('/validateUrl/:id', async (req, res) => {
             id: req.params.id,
             url,
         });
+        if (response && response._id) {
+            res.json({
+                code: 'vitruveo.studio.api.stores.validateUrl.failed',
+                message: 'URL already in use',
+                transaction: nanoid(),
+                data: false,
+            } as APIResponse);
+            return;
+        }
 
         res.json({
             code: 'vitruveo.studio.api.stores.validateUrl.success',
             message: 'Validate url success',
             transaction: nanoid(),
-            data: !response?._id,
+            data: true,
         } as APIResponse);
     } catch (error) {
         logger('Validate url failed: %O', error);
