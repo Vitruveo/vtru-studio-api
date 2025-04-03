cd2b4f5c2ce979e567f5dd7f5c0700ddfef02c16
Matheus Paice <paice.zinner@gmail.com>
2025-04-03 07:34:47 -0300

    feat: add service stripe to buy print license

--- src/constants/stripe.ts
+++ src/constants/stripe.ts
@@ -1,3 +1,1 @@
-export const STRIPE_SECRET_KEY =
-    process.env.STRIPE_SECRET_KEY ||
-    'sk_test_51R6yFgRrlxc7IWQQpf0TUz4OoGmEa7WxylLkXn3fFb5Pmrb3bAzvd7trwXv4pjPLgof0xsrxCGtYsN4FWUCSuPem00VWiickU2';
+export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || '';
